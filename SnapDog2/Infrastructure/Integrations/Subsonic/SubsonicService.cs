2d17f9459c46ef1b8997dc4d5476d6bcd7308636
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2025-08-24 11:55:05 +0200

    chore: add licensing information and header comments to various command, handler, notification, query, and service files

--- SnapDog2/Infrastructure/Integrations/Subsonic/SubsonicService.cs
+++ SnapDog2/Infrastructure/Integrations/Subsonic/SubsonicService.cs
@@ -1,3 +1,16 @@
+//
+// SnapDog
+// The Snapcast-based Smart Home Audio System with MQTT & KNX integration
+// Copyright (C) 2025 Fabian Schmieder
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
 namespace SnapDog2.Infrastructure.Integrations.Subsonic;
 
 using System;
