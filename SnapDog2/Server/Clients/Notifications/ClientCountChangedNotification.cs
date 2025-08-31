202e84b2999ebadda4936877a5402674b207cdb4
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2025-08-31 12:38:33 +0200

    feat: add notifications for client and zone count changes
    introduce ClientCountChangedNotification and ZoneCountChangedNotification records to publish notifications when the total number of clients and zones changes, respectively. Each notification includes a required Count property to represent the total number.

---
+++ SnapDog2/Server/Clients/Notifications/ClientCountChangedNotification.cs
@@ -0,0 +1,29 @@
+//
+// SnapDog
+// The Snapcast-based Smart Home Audio System with MQTT & KNX integration
+// Copyright (C) 2025 Fabian Schmieder
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+namespace SnapDog2.Server.Clients.Notifications;
+
+using Cortex.Mediator.Notifications;
+using SnapDog2.Shared.Attributes;
+
+/// <summary>
+/// Notification published when the total number of clients changes.
+/// </summary>
+[StatusId("CLIENT_COUNT")]
+public record ClientCountChangedNotification : INotification
+{
+    /// <summary>
+    /// Gets the total number of clients.
+    /// </summary>
+    public required int Count { get; init; }
+}
